1fa9f9e15a8348011cff56318a418db5d2e2156d
Garrett Fite <garrett.fite@gmail.com>
2022-01-17 15:54:30 -0600

    added WriteUpListItem react component

--- wright-tire-website/src/components/WriteUpContainer/WriteUpContainer.js
+++ wright-tire-website/src/components/WriteUpContainer/WriteUpContainer.js
@@ -7,48 +7,52 @@ import { WriteUpWorkButtons } from '../../components/WriteUpWorkButtons/WriteUpW
 export function WriteUpContainer(props) {
     return (
         <div className="write-up-container">
-             <WriteUpList className="col-1 test-border"></WriteUpList>
-            <WriteUpAdminButtons className="write-up-header col-2 test-border">
-                Header
-            </WriteUpAdminButtons>
-
-           
-
-            <WriteUpWorkButtons className="write-up-work-buttons col-3 test-border">
-                Work Buttons
-        </WriteUpWorkButtons>
-            <div className="write-up-sheet col-2 test-border">
-                Write up Sheet
-                Customer Name
-                Vehicle Info
-                <div className="test-line-work">
-                    <div className="test-line-work-title">
-                        Flat Repair
+            <WriteUpList className="col-1 write-up-list-grid-position test-border"></WriteUpList>
+            <div className="col-2 center">
+                <WriteUpAdminButtons className="write-up-header col-2 test-border">
+                    Header
+                </WriteUpAdminButtons>
+                <div className="write-up-sheet write-up-main-grid-position test-border">
+                    Write up Sheet
+                    Customer Name
+                    Vehicle Info
+                    <div className="test-line-work">
+                        <div className="test-line-work-title">
+                            Flat Repair
+                        </div>
+                        <div className="test-line-work-info">
+                            Passenger Rear
+                        </div>
+                        <div className="test-line-work-notes">
+                            Found nail, repaired
+                        </div>
                     </div>
-                    <div className="test-line-work-info">
-                        Passenger Rear
+                    <div className="test-line-work">
+                        <div className="test-line-work-title">
+                            LOF
+                        </div>
+                        <div className="test-line-work-info">
+                            6QT, Check Air Filter
+                        </div>
                     </div>
-                    <div className="test-line-work-notes">
-                        Found nail, repaired
-                    </div>
-                </div>
-                <div className="test-line-work">
-                    <div className="test-line-work-title">
-                        LOF
-                    </div>
-                    <div className="test-line-work-info">
-                        6QT, Check Air Filter
-                    </div>
-                </div>
-                <div className="test-line-work">
-                    <div className="test-line-work-title">
-                        Tire Light
-                    </div>
-                    <div className="test-line-work-info">
-                        Diag.
+                    <div className="test-line-work">
+                        <div className="test-line-work-title">
+                            Tire Light
+                        </div>
+                        <div className="test-line-work-info">
+                            Diag.
+                        </div>
                     </div>
                 </div>
             </div>
+
+
+
+
+            <WriteUpWorkButtons className="write-up-work-buttons col-3 test-border">
+                Work Buttons
+            </WriteUpWorkButtons>
+
         </div>
     )
 }
---
+++ wright-tire-website/src/components/WriteUpListItem/WriteUpListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+export function WriteUpListItem(props) {
+    return (
+        <button>
+            <div className="write-up-list-info-box">
+                <div>{props.name}</div>
+                <div>{props.vehicle}<div className={`car-color-icon ${props.color}`}></div></div>
+            </div>
+
+            <div className="test-waiter">{props.status}</div>
+        </button>
+    )
+}
