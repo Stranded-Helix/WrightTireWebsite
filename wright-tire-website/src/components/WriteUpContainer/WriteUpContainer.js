4c1adaefc9e0da26a30d7c0ac40d2dc2a9df6788
Garrett Fite <36345799+Stranded-Helix@users.noreply.github.com>
2022-01-16 19:56:44 -0600

    added framework components and low level css

---
+++ wright-tire-website/src/components/WriteUpContainer/WriteUpContainer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './styles.css';
+import { WriteUpList } from '../../components/WriteUpList/WriteUpList';
+import { WriteUpAdminButtons } from '../../components/WriteUpAdminButtons/WriteUpAdminButtoms';
+import { WriteUpWorkButtons } from '../../components/WriteUpWorkButtons/WriteUpWorkButtons';
+
+export function WriteUpContainer(props) {
+    return (
+        <div className="write-up-container">
+             <WriteUpList className="col-1 test-border"></WriteUpList>
+            <WriteUpAdminButtons className="write-up-header col-2 test-border">
+                Header
+            </WriteUpAdminButtons>
+
+           
+
+            <WriteUpWorkButtons className="write-up-work-buttons col-3 test-border">
+                Work Buttons
+        </WriteUpWorkButtons>
+            <div className="write-up-sheet col-2 test-border">
+                Write up Sheet
+                Customer Name
+                Vehicle Info
+                <div className="test-line-work">
+                    <div className="test-line-work-title">
+                        Flat Repair
+                    </div>
+                    <div className="test-line-work-info">
+                        Passenger Rear
+                    </div>
+                    <div className="test-line-work-notes">
+                        Found nail, repaired
+                    </div>
+                </div>
+                <div className="test-line-work">
+                    <div className="test-line-work-title">
+                        LOF
+                    </div>
+                    <div className="test-line-work-info">
+                        6QT, Check Air Filter
+                    </div>
+                </div>
+                <div className="test-line-work">
+                    <div className="test-line-work-title">
+                        Tire Light
+                    </div>
+                    <div className="test-line-work-info">
+                        Diag.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
